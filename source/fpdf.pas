252faae4b7406c34c7b4dd550e938a4831df2d13
Daniel Simões de Almeida <daniel@projetoacbr.com.br>
2023-04-28 15:54:47 -0300

    Correções de Erro de compilação em NetGen e 10.3
    Remoção de Hints

--- source/fpdf.pas
+++ source/fpdf.pas
@@ -37,13 +37,15 @@
 unit fpdf;
 
 // If you don't want the AnsiString vs String warnings to bother you
-{.$DEFINE REMOVE_CAST_WARN}
+//{$DEFINE REMOVE_CAST_WARN}
 
 {$IfNDef FPC}
   {$IFDEF REMOVE_CAST_WARN}
+   {$IF CompilerVersion >= 16}
     {$WARN IMPLICIT_STRING_CAST OFF}
     {$WARN IMPLICIT_STRING_CAST_LOSS OFF}
-  {$ENDIF}
+   {$IfEnd}
+  {$EndIf}
 {$EndIf}
 
 {$IfDef FPC}
@@ -68,8 +70,21 @@ unit fpdf;
 interface
 
 uses
-  Classes, Contnrs,
-  {$IfDef FPC}zstream{$Else}ZLib{$EndIf},
+  Classes,
+  {$IfDef FPC}
+   zstream, Contnrs
+  {$Else}
+   {$IfDef HAS_SYSTEM_GENERICS}
+    System.Generics.Collections, System.Generics.Defaults
+   {$Else}
+    {$IF CompilerVersion >= 16}
+     System.Contnrs
+    {$Else}
+     Contnrs
+    {$IfEnd},
+   {$EndIf}
+   ZLib
+  {$EndIf},
   SysUtils;
 
 const
