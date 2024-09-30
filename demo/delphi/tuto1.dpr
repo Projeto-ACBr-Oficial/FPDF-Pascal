8d1c5db3f3a0fda8de29f4dc35d989148b83567f
Daniel Simões de Almeida <daniel@projetoacbr.com.br>
2024-09-30 20:42:21 -0300

    Tuto1 changed for UTF8/ANSI tests on Linux / Windows

--- demo/delphi/tuto1.dpr
+++ demo/delphi/tuto1.dpr
@@ -10,9 +10,11 @@ begin
   pdf := TFPDF.Create();
   try
     pdf.SetCompression(False);
+    pdf.SetUTF8(False);      //  This file is ANSI CP1252 (check with NotePad++)
     pdf.AddPage();
     pdf.SetFont('Arial','B',16);
     pdf.Cell(40,10,'Hello World!');
+    pdf.Cell(60,10,'аимсз');
     pdf.SaveToFile(ExtractFilePath(ParamStr(0))+PathDelim+'tuto1-pas.pdf');
   finally
     pdf.Free;
