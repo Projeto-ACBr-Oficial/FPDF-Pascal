d0360eb22cd79276de8c306b0aec67f2190309ab
Daniel Simões de Almeida <daniel@projetoacbr.com.br>
2025-05-03 12:25:34 -0300

    Added FPDFPascal Package
    Added Package to allow use of Lazarus's OnlinePackageManage

---
+++ package/fpdfpascal.pas
@@ -0,0 +1,15 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit FPDFPascal;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  fpdf, fpdf_ext, fpdf_report;
+
+implementation
+
+end.
